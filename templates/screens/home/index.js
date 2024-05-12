a0b6577653d2df75567a0ba1ad87eebc5ac83d48
dhelafrank <ubongabasijerome8@gmail.com>
2024-05-12 12:55:55 +0100

    Instance: Links Added

--- templates/screens/home/index.js
+++ templates/screens/home/index.js
@@ -205,7 +205,7 @@ function main() {
         </a>
         <a
           class="inline-flex text-gray-50 h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-          href="https://github.com/dhelafrank/Frontend Boilerplate"
+          href="https://github.com/dhelafrank/EJS-Project-Boilerplate" target="_blank"
         >
           Github
         </a>
